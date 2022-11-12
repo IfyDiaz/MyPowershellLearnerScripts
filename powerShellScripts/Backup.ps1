27944a4742da11cb599f64ab5bff9895762bd976
IfyDiaz <emydiamond007@gmail.com>
2022-11-12 08:57:21 +0200

    error handling

--- powerShellScripts/Backup.ps1
+++ powerShellScripts/Backup.ps1
@@ -2,8 +2,24 @@
 
 Param(
   [string]$Path = './app',
-  [string]$DestinationPath = './'
+  [string]$DestinationPath = './',
+  [switch]$PathIsWebApp
 )
+
+If ($PathIsWebApp -eq $True) {
+   Try
+   {
+     $ContainsApplicationFiles = "$((Get-ChildItem $Path).Extension | Sort-Object -Unique)" -match  '\.js|\.html|\.css'
+
+     If ( -Not $ContainsApplicationFiles) {
+       Throw "Not a web app"
+     } Else {
+       Write-Host "Source files look good, continuing"
+     }
+   } Catch {
+    Throw "No backup created due to: $($_.Exception.Message)"
+   }
+}
 
 If (-Not (Test-Path $Path))
 {
